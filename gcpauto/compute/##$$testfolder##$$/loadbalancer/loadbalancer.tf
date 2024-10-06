ef88f0aea7c30e5960eb18ea7ca02eaf90ed0c5f
cpg-qe <swathi.g@servicenow.com>
2024-10-06 20:15:06 +0530

    Update loadbalancer.tf

--- gcpauto/compute/##$$testfolder##$$/loadbalancer/loadbalancer.tf
+++ gcpauto/compute/##$$testfolder##$$/loadbalancer/loadbalancer.tf
@@ -1,14 +1,14 @@
 resource "google_compute_address" "lb_ip" {
-  name   = "${var.address_name}-${random_hex.bucket_suffix[0].result}"
+  name   = "${var.address_name}-${random_id.bucket_suffix.hex}"
   region = var.region
 }
 
 resource "google_compute_http_health_check" "http_health_check" {
-  name               = "${var.health_check_name}-${random_hex.bucket_suffix[0].result}"
+  name               = "${var.health_check_name}-${random_id.bucket_suffix.hex}"
 }
 
 resource "google_compute_target_pool" "target_pool" {
-  name = "${var.target_pool_name}-${random_hex.bucket_suffix[0].result}"
+  name = "${var.target_pool_name}-${random_id.bucket_suffix.hex}"
 
   instances = var.instances
 
@@ -18,12 +18,11 @@ resource "google_compute_target_pool" "target_pool" {
 }
 
 resource "google_compute_forwarding_rule" "forwarding_rule" {
-  name       = "${var.forwarding_rule_name}-${random_hex.bucket_suffix[0].result}"
+  name       = "${var.forwarding_rule_name}-${random_id.bucket_suffix.hex}"
   target     = google_compute_target_pool.target_pool.self_link
   port_range = "80"
 }
 
-resource "random_hex" "bucket_suffix" {
-  count = var.bucket_count
-  length = 4  # Length of the random hex suffix
+resource "random_id" "bucket_suffix" {
+  byte_length = 4  # Adjust the length of the random string (in bytes)
 }
